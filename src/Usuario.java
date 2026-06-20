2bb40d1b4d8525c19d777a5086c17e989d7fb25f
SebasDV201 <170909718+SebasDV201@users.noreply.github.com>
2026-06-20 15:11:07 -0500

    Crear enum Rol y clase abstracta Usuario con atributos

---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+public abstract class Usuario{
+    
+    protected String codigoUnico ;
+    protected String cedula;
+    protected String nombres;
+    protected String apellidos;
+    protected String usuario;
+    protected String contrasena;
+    protected String correo;
+    protected Rol rol;
+
+}
