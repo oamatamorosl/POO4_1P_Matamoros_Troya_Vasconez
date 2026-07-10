f1015ea9e9e407c7890f6ecc26c8780a3165dadb
Oscar Matamoros <osalmalo2007@hotmail.com>
2026-07-10 03:01:54 -0500

    Corrige metodos en clase Aficionado

--- src/Aficionado.java
+++ src/Aficionado.java
@@ -4,69 +4,70 @@ public class Aficionado extends Usuario {
     private String celular;
     private String paisFavorito;
 
-    //Constructor clase Aficionado
-    public Aficionado(String codigoUnico, String cedula, String nombres, 
-                    String apellidos, String usuario, String contrasena, 
-                    String correo, String celular,String paisFavorito){
+    // Constructor clase Aficionado
+    public Aficionado(String codigoUnico, String cedula, String nombres,
+            String apellidos, String usuario, String contrasena,
+            String correo, String celular, String paisFavorito) {
 
-             //LLamando a constructor de clase padre           
-            super(codigoUnico,  cedula, nombres, 
-                 apellidos,  usuario, contrasena, 
-                 correo);
+        // LLamando a constructor de clase padre
+        super(codigoUnico, cedula, nombres,
+                apellidos, usuario, contrasena,
+                correo);
 
-            //Asignación de atributos propios de Aficionado
-            this.celular = celular;
-            this.paisFavorito = paisFavorito;
-            this.rol = Rol.A; // El rol de aficionado siempre es A
-}
+        // Asignación de atributos propios de Aficionado
+        this.celular = celular;
+        this.paisFavorito = paisFavorito;
+        this.rol = Rol.A; // El rol de aficionado siempre es A
+    }
 
-    // Implementación temporal: la lógica real depende de la clase Compra que aún no existe.
+    // Implementación temporal: la lógica real depende de la clase Compra que aún no
+    // existe.
     @Override
-    public void consultarEntradas(){
-        System.out.println("Consultando entradas como aficionado..."); 
+    public void consultarEntradas() {
+        System.out.println("Consultando entradas como aficionado...");
+    }
 
-public void consultarPartidos() { //HAY QUE HACER LA LOGICA DE ESTE METODO
-    System.out.println("Consultando partidos...");
-}
+    // temporal
 
-public void comprar(Partido partido, Zona zona, int cantidad, String tarjeta) {//HAY QUE HACER LA LOGICA DE ESTE METODO
-    System.out.println("Comprando entrada...");
-}
+    public void consultarPartidos() {
+        System.out.println("Consultando partidos...");
+    }
 
-public void comprar(Kit kit, String tarjeta) {//HAY QUE HACER LA LOGICA DE ESTE METODO
-    System.out.println("Comprando kit...");
-}
+    public void comprar(Partido partido, Zona zona, int cantidad, String tarjeta) {
+        System.out.println("Comprando entrada...");
+    }
 
-}
+    public void comprar(Kit kit, String tarjeta) {
+        System.out.println("Comprando kit...");
+    }
+    // Getters
 
-//Getters 
+    public String getCelular() {
+        return celular;
+    }
 
-public String getCelular(){
-    return celular;
-}
+    public String getPaisFavorito() {
+        return paisFavorito;
+    }
 
-public String getPaisFavorito(){
-    return paisFavorito;
-}
+    // Setters
 
-//Setters
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
 
-public void setCelular(String celular){
-    this.celular = celular;
-}
+    public void setPaisFavorito(String paisFavorito) {
+        this.paisFavorito = paisFavorito;
+    }
 
-public void setPaisFavorito(String paisFavorito){
-    this.paisFavorito = paisFavorito;
-}
-
-// Sobrescritura de toString() con super.toString()
+    // Sobrescritura de toString() con super.toString()
 
     @Override
-    public String toString(){
-    return super.toString() + 
-    ", Datos únicos de Aficionado: {Celular = " +this.celular+
-    ", País Favorito = " +this.paisFavorito+ "}";
+    public String toString() {
+        return super.toString() +
+                ", Datos únicos de Aficionado: {Celular = " + this.celular +
+                ", País Favorito = " + this.paisFavorito + "}";
 
-}
+    }
 
 }
